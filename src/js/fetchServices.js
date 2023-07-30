49c4a58915a9908dc6b15418a201fc8108c62150
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2023-07-30 15:10:05 +0300

    changed serviceFetch to onload

--- src/js/fetchServices.js
+++ src/js/fetchServices.js
@@ -1,17 +1,12 @@
 const BASE_URL = 'https://sigma-hw-04.lm.r.appspot.com/api/service/';
 
-export async function fetchAllServices({ page, limit }) {
-  const savedServices = localStorage.getItem('all');
-  if (savedServices) {
-    return JSON.parse(savedServices);
-  }
+async function fetchAllServices({ page, limit }) {
   try {
     const response = await fetch(`${BASE_URL}?page=${page}&limit=${limit}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    localStorage.setItem('all', JSON.stringify(data));
     return data;
   } catch (error) {
     console.error('There was an error fetching the services:', error);
@@ -19,21 +14,22 @@ export async function fetchAllServices({ page, limit }) {
   }
 }
 
-export async function fetchServicesByCategory({ categoryName, page, limit }) {
-  const savedServices = localStorage.getItem(categoryName);
-  if (savedServices) {
-    return JSON.parse(savedServices);
-  }
-  try {
-    const response = await fetch(`${BASE_URL}${categoryName}?page=${page}&limit=${limit}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    localStorage.setItem(categoryName, JSON.stringify(data));
-    return data;
-  } catch (error) {
-    console.error('There was an error fetching the services:', error);
-    throw error;
+async function saveServicesByCategory() {
+  const { services } = await fetchAllServices({ page: 1, limit: 12 });
+  if (services) {
+    const interior = services
+      .filter(service => service.serviceCategory === 'interior_design')
+      .slice(0, 3);
+    const architecture = services
+      .filter(service => service.serviceCategory === 'architecture')
+      .slice(0, 3);
+    const planning = services.filter(service => service.serviceCategory === 'planning').slice(0, 3);
+    const all = [...interior, ...architecture, ...planning];
+    localStorage.setItem('all', JSON.stringify(all));
+    localStorage.setItem('interior', JSON.stringify(interior));
+    localStorage.setItem('architecture', JSON.stringify(architecture));
+    localStorage.setItem('planning', JSON.stringify(planning));
   }
 }
+
+saveServicesByCategory();
--- src/js/populateServices.js
+++ src/js/populateServices.js
@@ -1,4 +1,3 @@
-import { fetchAllServices, fetchServicesByCategory } from './fetchServices';
 import architectureImg from '../images/architecture.svg';
 import interiorImg from '../images/interior.svg';
 import planningImg from '../images/planning.svg';
@@ -10,103 +9,79 @@ const refs = {
   interiorBtn: document.querySelector('#interior'),
   planningBtn: document.querySelector('#planning'),
 };
-
-refs.allBtn.addEventListener('click', handleAllClick);
-refs.architectureBtn.addEventListener('click', handleFetchByCategory);
-refs.interiorBtn.addEventListener('click', handleFetchByCategory);
-refs.planningBtn.addEventListener('click', handleFetchByCategory);
-
+const { serviceContainer, allBtn, architectureBtn, interiorBtn, planningBtn } = refs;
 let currentFilter = null;
+
+allBtn.addEventListener('click', handleAllClick);
+architectureBtn.addEventListener('click', handleFetchByCategory);
+interiorBtn.addEventListener('click', handleFetchByCategory);
+planningBtn.addEventListener('click', handleFetchByCategory);
 
 function handleAllClick(e) {
   if (currentFilter === e.target.id) {
     e.target.classList.remove('service__button_clicked');
     currentFilter = null;
-    refs.serviceContainer.innerHTML = '';
+    serviceContainer.innerHTML = '';
   } else {
-    refs.architectureBtn.classList.remove('service__button_clicked');
-    refs.interiorBtn.classList.remove('service__button_clicked');
-    refs.planningBtn.classList.remove('service__button_clicked');
+    architectureBtn.classList.remove('service__button_clicked');
+    interiorBtn.classList.remove('service__button_clicked');
+    planningBtn.classList.remove('service__button_clicked');
     currentFilter = e.target.id;
     e.target.classList.add('service__button_clicked');
-    fetchAllServices({ page: 1, limit: 9 })
-      .then(data => {
-        const markup = data.services
-          .map(service => {
-            let imgPath;
-            switch (service.serviceCategory) {
-              case 'architecture':
-                imgPath = architectureImg;
-                break;
-              case 'interior_design':
-                imgPath = interiorImg;
-                break;
-              case 'planning':
-                imgPath = planningImg;
-                break;
-            }
-            return `<div class="service__post"><img class="service__icon" src=${imgPath} width="45" height="35" alt="icon"/><div class="service__text-wrapper"><p class="service__name">${service.serviceName}</p><p class="service__description">${service.serviceDescription}</p></div></div>`;
-          })
-          .join('');
-
-        refs.serviceContainer.innerHTML = markup;
+    const services = JSON.parse(localStorage.getItem('all'));
+    const markup = services
+      .map(service => {
+        let imgPath;
+        switch (service.serviceCategory) {
+          case 'architecture':
+            imgPath = architectureImg;
+            break;
+          case 'interior_design':
+            imgPath = interiorImg;
+            break;
+          case 'planning':
+            imgPath = planningImg;
+            break;
+        }
+        return `<div class="service__post"><img class="service__icon" src=${imgPath} width="45" height="35" alt="icon"/><div class="service__text-wrapper"><p class="service__name">${service.serviceName}</p><p class="service__description">${service.serviceDescription}</p></div></div>`;
       })
-      .catch(error => {
-        console.error('There was an error processing the services:', error);
-      });
+      .join('');
+
+    serviceContainer.innerHTML = markup;
   }
 }
 
 function handleFetchByCategory(e) {
-  let categoryName = '';
-  switch (e.target.id) {
-    case 'architecture':
-      categoryName = e.target.id;
-      break;
-    case 'interior':
-      categoryName = 'interior_design';
-      break;
-    case 'planning':
-      categoryName = e.target.id;
-      break;
-    default:
-      break;
-  }
   if (currentFilter === e.target.id) {
     e.target.classList.remove('service__button_clicked');
     currentFilter = null;
-    refs.serviceContainer.innerHTML = '';
+    serviceContainer.innerHTML = '';
   } else {
-    refs.allBtn.classList.remove('service__button_clicked');
-    refs.architectureBtn.classList.remove('service__button_clicked');
-    refs.interiorBtn.classList.remove('service__button_clicked');
-    refs.planningBtn.classList.remove('service__button_clicked');
+    allBtn.classList.remove('service__button_clicked');
+    architectureBtn.classList.remove('service__button_clicked');
+    interiorBtn.classList.remove('service__button_clicked');
+    planningBtn.classList.remove('service__button_clicked');
     currentFilter = e.target.id;
     e.target.classList.add('service__button_clicked');
-    fetchServicesByCategory({ categoryName, page: 1, limit: 3 })
-      .then(data => {
-        const markup = data.services
-          .map(service => {
-            let imgPath;
-            switch (service.serviceCategory) {
-              case 'architecture':
-                imgPath = architectureImg;
-                break;
-              case 'interior_design':
-                imgPath = interiorImg;
-                break;
-              case 'planning':
-                imgPath = planningImg;
-                break;
-            }
-            return `<div class="service__post"><img class="service__icon" src=${imgPath} width="45" height="35" alt="icon"/><div class="service__text-wrapper"><p class="service__name">${service.serviceName}</p><p class="service__description">${service.serviceDescription}</p></div></div>`;
-          })
-          .join('');
-
-        refs.serviceContainer.innerHTML = markup;
+    const services = JSON.parse(localStorage.getItem(e.target.id));
+    const markup = services
+      .map(service => {
+        let imgPath;
+        switch (service.serviceCategory) {
+          case 'architecture':
+            imgPath = architectureImg;
+            break;
+          case 'interior_design':
+            imgPath = interiorImg;
+            break;
+          case 'planning':
+            imgPath = planningImg;
+            break;
+        }
+        return `<div class="service__post"><img class="service__icon" src=${imgPath} width="45" height="35" alt="icon"/><div class="service__text-wrapper"><p class="service__name">${service.serviceName}</p><p class="service__description">${service.serviceDescription}</p></div></div>`;
       })
-      .catch(error => {
-        console.error('There was an error processing the services:', error);
-      });
+      .join('');
+
+    serviceContainer.innerHTML = markup;
   }
 }
